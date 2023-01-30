bb950b07fb6893a3f8fb6b6f6418ce93023d6629
DaviAugustoMS     <jeffersonrochadasilva667@gmail.com>
2023-01-30 15:28:16 -0300

    eventos no vue3

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <h1>app</h1>
+  <p>{{}}</p>
+  <Children @emitir="send" />
 </template>
+<script setup lang="ts">
+import Children from "@/components/Children.vue";
 
+function send(data: any) {
+  console.log(data);
+}
+</script>
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  font-family: sans-serif;
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/Children.vue
@@ -0,0 +1,11 @@
+<template>
+  <button @click="emitirEvento()">Send / Enviar</button>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(["emitir"]);
+
+function emitirEvento() {
+  emit("emitir", { nam: "Jefferson", age: 30 });
+}
+</script>
