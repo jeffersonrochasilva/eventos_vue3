600d9fa549609e2cf8747ab66b42f050b93b3937
DaviAugustoMS     <jeffersonrochadasilva667@gmail.com>
2023-01-30 16:56:08 -0300

    adicao

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <h1>app</h1>
-  <p>{{}}</p>
-  <Children @emitir="send" />
+  <p>{{ twoName }}</p>
+  <p>{{ objeto.name }}</p>
+  <p>{{ objeto.age }}</p>
+
+  <Children
+    :testando="testando"
+    @emitir="send"
+    @eventoDeSobreNome="twoName = $event"
+  />
 </template>
 <script setup lang="ts">
+import { Ref, ref } from "vue";
 import Children from "@/components/Children.vue";
+const twoName: Ref<string> = ref("");
+const testando: Ref<string> = ref("testando");
+const objeto: Ref<any> = ref({});
 
 function send(data: any) {
+  objeto.value = data;
   console.log(data);
+}
+function getLastName(res: any) {
+  twoName.value = res;
 }
 </script>
 <style>
--- src/components/Children.vue
+++ src/components/Children.vue
@@ -1,11 +1,23 @@
 <template>
-  <button @click="emitirEvento()">Send / Enviar</button>
+  <button @click="emitirEvento()">Send / objeto</button>
+  <button @click="emitirnovoEvento()">Send / sobre Nome</button>
+  <h2>{{ props.testando }}</h2>
 </template>
 
 <script setup lang="ts">
-const emit = defineEmits(["emitir"]);
+import { Ref, ref } from "vue";
+const nome: Ref<string> = ref("Jefferson");
+
+const emit = defineEmits(["emitir", "eventoDeSobreNome"]);
+const props = defineProps<{
+  testando: String;
+}>();
 
 function emitirEvento() {
-  emit("emitir", { nam: "Jefferson", age: 30 });
+  emit("emitir", { name: "Jefferson", age: 30 });
+}
+
+function emitirnovoEvento() {
+  emit("eventoDeSobreNome", nome.value);
 }
 </script>
